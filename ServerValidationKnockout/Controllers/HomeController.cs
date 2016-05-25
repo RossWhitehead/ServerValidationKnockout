29ba67ac541c65a85714adff23818040689db583
Ross Whitehead <rossshaggywhitehead@hotmail.com>
2016-05-25 21:10:41 +0100

    Add EntLib logging and ApplicationInsights. EntlibTraceListener

--- ServerValidationKnockout/Controllers/HomeController.cs
+++ ServerValidationKnockout/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
 
 namespace ServerValidationKnockout.Controllers
 {
@@ -10,6 +11,21 @@ namespace ServerValidationKnockout.Controllers
     {
         public ActionResult Index()
         {
+            Logger.SetLogWriter(new LogWriterFactory().Create());
+
+            Logger.Write(new LogEntry(
+                message: "logging message",
+                category: "category 1",
+                priority: 1,
+                eventId: 123,
+                severity: System.Diagnostics.TraceEventType.Error,
+                title: "logging title",
+                properties: null
+                ));
+
+
+            //throw new Exception("Exception in index");
+
             return View();
         }
 
